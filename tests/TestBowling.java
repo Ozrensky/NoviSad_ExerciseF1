9813d385db817fd1841d80c2fc686ec2238450de
somename <someemail>
2017-05-12 11:59:20 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -9,4 +9,10 @@ public class TestBowling {
 		fail("Not yet implemented");
 	}
 
+	@Test
+	public void test_Frame_Score(){
+		Frame f = new Frame(3, 4);
+		
+		assertEquals("Greska ako nije dobijen ocekivan zbir", 7, f.score());
+	}
 }
